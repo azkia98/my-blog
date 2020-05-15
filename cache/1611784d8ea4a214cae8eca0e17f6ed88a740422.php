6d7e064bf8e72af5e19e8d305831d356ca8a6150
Mahdi Ebrahimi <azkia@iran.ir>
2020-05-16 01:01:14 +0430

    change book icon

--- cache/1611784d8ea4a214cae8eca0e17f6ed88a740422.php
+++
@@ -1,3 +0,0 @@
-<?php $__env->startSection('content'); ?><p>This is the start of your first blog post.</p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('_layouts.post', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,7 +32,7 @@
             <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center">
                     <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
-                        <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
+                        <img class="h-8 md:h-10 mr-3" src="/assets/img/book(1).svg" alt="{{ $page->siteName }} logo" />
 
                         <h1 class="text-lg md:text-2xl text-blue-800 font-semibold hover:text-blue-600 my-0">{{ $page->siteName }}</h1>
                     </a>
@@ -61,10 +61,16 @@
                 </li>
 
                 <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
+                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>,
+                    <a href="https://www.flaticon.com/" title="Flaticon">flaticon</a>
                     and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
                 </li>
             </ul>
+
+
+            <div class="hidden">
+                Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from 
+            </div>
         </footer>
 
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
