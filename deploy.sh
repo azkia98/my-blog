53b9b331da35070527440cef217d9f4a7f86a3d8
Mahdi Ebrahimi <azkia@iran.ir>
2020-07-09 22:15:59 +0430

    create the deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git add build_production && git commit -m "Build for deploy"
+
+git add -f build_production && git commit -m "Build for deploy"
+
+git subtree push --prefix build_production origin gh-pages
