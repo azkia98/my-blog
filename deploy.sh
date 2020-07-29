835bac0b662d98204c50e9d3e2098432ee6825bf
Mahdi Ebrahimi <azkia@iran.ir>
2020-07-29 22:33:29 +0430

    1

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-npm run production
+# npm run production
 
 git add build_production && git commit -m "Build for deploy"
 
 git add -f build_production && git commit -m "Build for deploy"
 
-git subtree push -f --prefix build_production origin gh-pages
+git subtree push --prefix build_production origin gh-pages
